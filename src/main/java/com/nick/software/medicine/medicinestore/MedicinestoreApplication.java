7b5089b5a51b91a0326555d5b43af3ff6ee92b77
NickThe1 <nikolay.kotlyarov1@gmail.com>
2021-03-11 21:38:18 +0600

    init

---
+++ src/main/java/com/nick/software/medicine/medicinestore/MedicinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.nick.software.medicine.medicinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinestoreApplication.class, args);
+    }
+
+}
